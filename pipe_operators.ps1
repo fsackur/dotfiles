cc5abb80afe3d73bd90341205720fed228f7ea56
Freddie Sackur <github@dustyfox.uk>
2020-12-24 16:38:38 +0000

    Allow text operators to work in pipeline

---
+++ pipe_operators.ps1
@@ -0,0 +1,87 @@
+function split
+{
+    param
+    (
+        [Parameter(ValueFromPipeline)]
+        [string[]]$InputObject,
+
+        [Parameter(Mandatory, Position = 0, ValueFromRemainingArguments)]
+        [ValidateCount(1, 3)]
+        [string[]]$args
+    )
+
+    end
+    {
+        $input -split $args
+    }
+}
+
+function join
+{
+    param
+    (
+        [Parameter(ValueFromPipeline)]
+        [string[]]$InputObject,
+
+        [Parameter(Mandatory, Position = 0, ValueFromRemainingArguments)]
+        [ValidateCount(1, 3)]
+        [string[]]$args
+    )
+
+    end
+    {
+        $input -join $args
+    }
+}
+
+function replace
+{
+    param
+    (
+        [Parameter(ValueFromPipeline)]
+        [string[]]$InputObject,
+
+        [Parameter(Mandatory, Position = 0, ValueFromRemainingArguments)]
+        [ValidateCount(1, 2)]
+        [string[]]$args
+    )
+
+    end
+    {
+        $input -replace $args
+    }
+}
+
+function match
+{
+    param
+    (
+        [Parameter(ValueFromPipeline)]
+        [string[]]$InputObject,
+
+        [Parameter(Mandatory, Position = 0)]
+        [string]$Pattern
+    )
+
+    end
+    {
+        $input -match $Pattern
+    }
+}
+
+function notmatch
+{
+    param
+    (
+        [Parameter(ValueFromPipeline)]
+        [string[]]$InputObject,
+
+        [Parameter(Mandatory, Position = 0)]
+        [string]$Pattern
+    )
+
+    end
+    {
+        $input -notmatch $Pattern
+    }
+}
