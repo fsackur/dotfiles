b17de2efe4b5c92b715970a67871c30068bc48e7
Freddie Sackur <Freddie.Sackur@rackspace.co.uk>
2021-01-27 10:15:19 +0000

    WIP

--- New-Symlinks.ps1
+++ New-Symlinks.ps1
@@ -1,12 +1,20 @@
 
+$_HOME = 'C:\Users\mich8638'
+
 $SourcePath = Join-Path $PSScriptRoot 'Code'
 $DotFiles   = Get-ChildItem $SourcePath
 $DotFiles   | ForEach-Object {
-    cmd /c "mklink `"$HOME\AppData\Roaming\Code\User\$($_.Name)`" `"$($_.FullName)`""
+    cmd /c "mklink `"$_HOME\AppData\Roaming\Code\User\$($_.Name)`" `"$($_.FullName)`""
 }
 
 $SourcePath = Join-Path $PSScriptRoot 'Code - Insiders'
 $DotFiles   = Get-ChildItem $SourcePath
 $DotFiles   | ForEach-Object {
-    cmd /c "mklink `"$HOME\AppData\Roaming\Code - Insiders\User\$($_.Name)`" `"$($_.FullName)`""
+    cmd /c "mklink `"$_HOME\AppData\Roaming\Code - Insiders\User\$($_.Name)`" `"$($_.FullName)`""
+}
+
+$SourcePath = Join-Path $PSScriptRoot 'ConEmu'
+$DotFiles   = Get-ChildItem $SourcePath
+$DotFiles   | ForEach-Object {
+    cmd /c "mklink `"$_HOME\AppData\Roaming\$($_.Name)`" `"$($_.FullName)`""
 }
