be0e4ceede012be066aa86d6da99e7480b89ddcd
Freddie Sackur <github@dustyfox.uk>
2021-02-02 21:49:11 +0000

    Bundle module stuff

---
+++ PSHelpers/ModuleLoad.ps1
@@ -0,0 +1,139 @@
+﻿function Export-PrivateModuleMember
+{
+    <#
+        .SYNOPSIS
+        Exports non-exported functions from a module.
+    #>
+    [CmdletBinding()]
+    [OutputType([void])]
+    param
+    (
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
+        [string]$Module,
+
+        [switch]$ExportVariables
+    )
+
+    process
+    {
+        [psmoduleinfo]$Module = Get-Module $Module -ErrorAction Stop | Select-Object -First 1
+        & $Module {
+            $Module = $args[0]
+            $ExportVariables = $args[1]
+
+            $Commands = Get-Command -Module $Module
+            $ExportTable = $Module.ExportedCommands
+            $PrivateCommands = $Commands | Where-Object {-not $ExportTable.ContainsKey($_.Name)}
+
+            foreach ($Command in $PrivateCommands)
+            {
+                Write-Verbose "Exporting private function '$($Command.Name)'"
+                Set-Content function:\Global:$($Command.Name) $Command.ScriptBlock # $Module.NewBoundScriptBlock([scriptblock]::Create($Command.Definition))
+            }
+
+            if ($ExportVariables)
+            {
+                $Variables = Get-Variable -Scope 1
+                $GlobalVariableLookup = Get-Variable -Scope 2 | Group-Object Name -AsHashTable
+                $ModuleVariables = $Variables |
+                    Where-Object {-not $GlobalVariableLookup.ContainsKey($_.Name)}
+
+                foreach ($Variable in $ModuleVariables)
+                {
+                    Write-Verbose "Exporting private variable '$($Variable.Name)'"
+                    Set-Variable -Name $Variable.Name -Value $Variable.Value -Scope 2
+                }
+            }
+
+        } $Module $ExportVariables
+    }
+}
+
+
+$ArgumentCompleterSplat = @{
+    CommandName   = 'Export-PrivateModuleMember'
+    ParameterName = 'Module'
+    ScriptBlock   = {
+        param
+        (
+            $commandName,
+            $parameterName,
+            $wordToComplete,
+            $commandAst,
+            $fakeBoundParameters
+        )
+
+        Get-Module |
+            Select-Object -ExpandProperty Name |
+            Sort-Object -Unique |
+            Where-Object {$_ -like "$wordToComplete*"}
+    }
+}
+Register-ArgumentCompleter @ArgumentCompleterSplat
+
+
+
+function Reload-Module
+{
+    <#
+        .SYNOPSIS
+        Imports module from anywhere within the module's folder structure in PowerShell.
+
+        .DESCRIPTION
+        Imports the module that is currently being tested within the PowerShell session's directory location. The module under
+        test will be imported so long as the PowerShell directory location is set to any location within the testing module's directory.
+
+        .OUTPUTS
+        [PSModuleInfo[]]
+
+        .EXAMPLE
+        Set-Location C:\Githubdata\Foobar
+        Import-ModuleUnderTest
+
+        ModuleType Version    Name                                ExportedCommands
+        ---------- -------    ----                                ----------------
+        Script     1.7.0.0    FooBar                              {Get-ModuleVersion, Get-TotalRam, Invoke-FooBar}
+
+        Imports the module folder you run the command from, in this case the Foobar directory.
+
+        .EXAMPLE
+        Set-Location C:\Githubdata\Foobar\Private
+        Import-ModuleUnderTest
+
+        ModuleType Version    Name                                ExportedCommands
+        ---------- -------    ----                                ----------------
+        Script     1.7.0.0    FooBar                              {Get-ModuleVersion, Get-TotalRam, Invoke-FooBar}
+
+        Will traverse the directory to the Module's root directory and imports the module located in the that directory.
+
+    #>
+
+    [CmdletBinding()]
+    [OutputType([PSModuleInfo])]
+
+    $ModuleBase = $PWD.Path
+
+    while ($ModuleBase -NotMatch '^\w:(\\?)$')
+    {
+        $ModuleName = Split-Path -Path $ModuleBase -Leaf
+
+        # Handle versioned module folders
+        if ($ModuleName -Match '^\d+(\.\d+){2,3}$')
+        {
+            $ModuleName = Split-Path -Path (Split-Path -Path $ModuleBase) -Leaf
+        }
+
+        $Psd1Path = Join-Path -Path $ModuleBase -ChildPath "$ModuleName.psd1"
+
+        if (Test-Path -Path $Psd1Path)
+        {
+            # Re-import module
+            Import-Module -Name $Psd1Path -Force -DisableNameChecking -PassThru -Global
+            return
+        }
+
+        $ModuleBase = Split-Path -Path $ModuleBase -Parent
+    }
+
+    Write-Warning "Not in a Powershell module directory, no module imported"
+}
