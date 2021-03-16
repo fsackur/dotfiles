ec2eb64b64eb535beabb455d680a70e19390f574
Freddie Sackur <github@dustyfox.uk>
2021-03-16 12:05:40 +0000

    Fixed IS_VSCODE on linux

--- PSHelpers/Env.ps1
+++ PSHelpers/Env.ps1
@@ -1,13 +1,19 @@
 
-$Global:IS_PS_CORE = $PSVersionTable.PSVersion.Major -ge 6
-$Global:IS_VSCODE = (
-    (-not $IS_PS_CORE -and (Get-Process -Id (Get-CimInstance Win32_Process -Filter "ProcessId = $PID").ParentProcessId).ProcessName -match '^Code( - Insiders)?$') -or
-    ($IS_PS_CORE -and (Get-Process -Id $PId).Parent.ProcessName -match '^Code( - Insiders)?$')
-)
 $Global:IS_ISE     = $Host.Name -eq 'Windows PowerShell ISE Host'
 $Global:IS_WSL     = [bool]$env:WSL_DISTRO_NAME
 $Global:IS_WINDOWS = [Environment]::OSVersion.Platform -match 'Win'
 $Global:IS_LINUX   = [Environment]::OSVersion.Platform -match 'Unix'
+$Global:IS_PS_CORE = $PSVersionTable.PSVersion.Major -ge 6
+
+$ParentProcess = if ($IS_PS_CORE)
+{
+    (Get-Process -Id $PId).Parent
+}
+else
+{
+    Get-Process -Id (Get-CimInstance Win32_Process -Filter "ProcessId = $PID").ParentProcessId
+}
+$Global:IS_VSCODE = $ParentProcess.ProcessName -match '^node|(Code( - Insiders)?)$'
 
 if ($Global:IS_WINDOWS)
 {
