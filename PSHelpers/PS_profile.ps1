c37718d2247aca2884612d6adaff4dc9fcce0e43
Freddie Sackur <github@dustyfox.uk>
2021-04-26 16:46:42 +0100

    Get-ChildItem shows hidden items by default

--- PSHelpers/PS_profile.ps1
+++ PSHelpers/PS_profile.ps1
@@ -2,6 +2,7 @@
 if (-not $PSDefaultParameterValues) {$Global:PSDefaultParameterValues = @{}}
 foreach ($Kvp in ([ordered]@{
     'Out-Default:OutVariable' = '+LastOutput'
+    'Get-ChildItem:Force'     = $true
 }).GetEnumerator())
 {
     $Global:PSDefaultParameterValues[$Kvp.Key] = $Kvp.Value
