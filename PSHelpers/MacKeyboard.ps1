4bb85139ae63164423301fd9e6c197d100a99bf5
Freddie Sackur <github@dustyfox.uk>
2022-10-18 16:35:08 +0100

    Use Karabiner CLI for profile selection

--- PSHelpers/MacKeyboard.ps1
+++ PSHelpers/MacKeyboard.ps1
@@ -33,6 +33,24 @@ function Invoke-Karabiner
     & (Get-KarabinerCommand) "--$Command" $ArgumentList
 }
 
+function Switch-KarabinerProfile
+{
+    [Diagnostics.CodeAnalysis.SuppressMessage('PSAvoidAssignmentToAutomaticVariable', '')]
+    param
+    (
+        [Parameter(Mandatory, Position = 0)]
+        [ArgumentCompleter({
+            $Current = Invoke-Karabiner show-current-profile-name
+            $All = Invoke-Karabiner list-profile-names
+            $Names = (@($All) -ne $Current), $Current | Write-Output
+            $Names -replace '.*\s.*', "'`$0'"
+        })]
+        $Profile
+    )
+
+    Invoke-Karabiner select-profile $Profile
+}
+
 function Get-KarabinerConfig
 {
     $Config = Get-KarabinerConfigPath | Resolve-Path | Get-Content | ConvertFrom-Json
@@ -54,11 +72,26 @@ function Set-KarabinerConfig
 
 function Get-KarabinerProfile
 {
+    [CmdletBinding(DefaultParameterSetName = 'Current')]
     param
     (
+        [Parameter(ParameterSetName = 'ByName', Mandatory, Position = 0)]
+        [ArgumentCompleter({(Invoke-Karabiner list-profile-names) -replace '.*\s.*', "'`$0'"})]
         [SupportsWildcards()]
-        $Name = '*'
+        $Name,
+
+        [Parameter(ParameterSetName = 'All')]
+        [switch]$All
     )
+
+    if ($PSCmdlet.ParameterSetName -eq 'Current')
+    {
+        $Name = Invoke-Karabiner show-current-profile-name
+    }
+    elseif ($PSCmdlet.ParameterSetName -eq 'All')
+    {
+        $Name = '*'
+    }
 
     $Config = Get-KarabinerConfig
     $Profiles = $Config.profiles | Where-Object Name -like $Name
@@ -72,7 +105,7 @@ function Set-KarabinerProfile
     param
     (
         [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
-        $Profile
+        [object]$Profile
     )
 
     begin
@@ -105,15 +138,26 @@ function Get-KarabinerRule
     [Diagnostics.CodeAnalysis.SuppressMessage('PSAvoidAssignmentToAutomaticVariable', '')]
     param
     (
-        [Parameter(Mandatory, Position = 0)]
+        [Parameter(Position = 0)]
+        [ArgumentCompleter({(Invoke-Karabiner list-profile-names) -replace '.*\s.*', "'`$0'"})]
         $Profile,
 
         [Parameter(Position = 1)]
+        [ArgumentCompleter({
+            param ($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+            $fakeBoundParameters.Remove($parameterName)
+            $Rules = Get-KarabinerRule @fakeBoundParameters
+            $Completions = (@($Rules.description) -ilike "$wordToComplete*"), (@($Rules.description) -ilike "*?$wordToComplete*") | Write-Output
+            # Write-Host $wordToComplete
+            # $Completions
+            $Completions -replace '.*\s.*', "'`$0'"
+        })]
         [SupportsWildcards()]
         $Description = '*'
     )
 
-    $_Profile = Get-KarabinerProfile $Profile
+    $ProfileParams = if ($Profile) {@{Name = $Profile}} else {@{}}
+    $_Profile = Get-KarabinerProfile @ProfileParams
     $Rules = $_Profile.complex_modifications.rules | Where-Object Description -like $Description
     $Rules | ForEach-Object {$_.PSTypeNames.Insert(0, 'KarabinerRule')}
     $Rules
@@ -124,16 +168,17 @@ function Set-KarabinerRule
     [Diagnostics.CodeAnalysis.SuppressMessage('PSAvoidAssignmentToAutomaticVariable', '')]
     param
     (
-        [Parameter(Mandatory, Position = 0)]
+        [ArgumentCompleter({(Invoke-Karabiner list-profile-names) -replace '.*\s.*', "'`$0'"})]
         $Profile,
 
-        [Parameter(Mandatory, Position = 1, ValueFromPipeline)]
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
         $Rule
     )
 
     begin
     {
-        $_Profile = Get-KarabinerProfile $Profile
+        $ProfileParams = if ($Profile) {@{Name = $Profile}} else {@{}}
+        $_Profile = Get-KarabinerProfile @ProfileParams
         $Rules = $_Profile.complex_modifications.rules
     }
 
@@ -157,17 +202,3 @@ function Set-KarabinerRule
         $_Profile | Set-KarabinerProfile
     }
 }
-
-$KarabinerProfileCompleter = {
-    param ($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
-
-    if (-not $Script:KarabinerProfiles)
-    {
-        $Script:KarabinerProfiles = (Get-KarabinerConfig).profiles.Name
-    }
-    $Completions = (@($Script:KarabinerProfiles) -like "$wordToComplete*"), (@($Script:KarabinerProfiles) -like "*.$wordToComplete*") | Write-Output
-    $Completions -replace '.*\s.*', "'`$0'"
-}
-
-Register-ArgumentCompleter -CommandName Get-KarabinerProfile, Set-KarabinerProfile -ParameterName Name -ScriptBlock $KarabinerProfileCompleter
-Register-ArgumentCompleter -CommandName Get-KarabinerRule, Set-KarabinerRule -ParameterName Profile -ScriptBlock $KarabinerProfileCompleter
