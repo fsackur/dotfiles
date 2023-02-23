d4b46360ebf32517df170de3cfbff875b2bbadd1
Freddie Sackur <github@dustyfox.uk>
2023-02-23 11:03:05 +0000

    Add work_profile

--- .chezmoitemplates/profile.ps1
+++ .chezmoitemplates/profile.ps1
@@ -1,4 +1,9 @@
-. {{ .chezmoi.sourceDir }}/PSHelpers/Console.ps1
-. {{ .chezmoi.sourceDir }}/PSHelpers/git_helpers.ps1
-. {{ .chezmoi.sourceDir }}/PSHelpers/pipe_operators.ps1
-. {{ .chezmoi.sourceDir }}/PSHelpers/ModuleLoad.ps1
+. "{{ .chezmoi.sourceDir }}/PSHelpers/Console.ps1"
+. "{{ .chezmoi.sourceDir }}/PSHelpers/git_helpers.ps1"
+. "{{ .chezmoi.sourceDir }}/PSHelpers/pipe_operators.ps1"
+. "{{ .chezmoi.sourceDir }}/PSHelpers/ModuleLoad.ps1"
+
+if (Test-Path "{{ .chezmoi.sourceDir }}/Work/work_profile.ps1")
+{
+    . "{{ .chezmoi.sourceDir }}/Work/work_profile.ps1"
+}
