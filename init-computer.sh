bca0a3b14f86c8a55b9f3b5b5d44b6c3eefa7293
Freddie Sackur <github@dustyfox.uk>
2023-08-12 21:03:13 +0100

    Don't rely on homebrew on ubuntu

--- init-computer.sh
+++ init-computer.sh
@@ -4,42 +4,60 @@
 #       bash -c "$(curl -fsSL https://raw.githubusercontent.com/fsackur/dotfiles/chezmoi/init-computer.sh)"
 #
 
-which brew || \
-    sudo bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && \
-    (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ~/.profile && \
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
+case $(uname -v) in
+    *Ubuntu*|*Debian*)
+        jq_installer="apt install jq"
+        bitwarden_installer="apt install snapd && snap install bw"
+        ansible_installer="apt install ansible";;
+    *)
+        echo "Distro not supported."
+        exit 1;;
+esac
 
-which git || brew install git
-which chezmoi || brew install chezmoi
+if [ ! "$(which jq)" ]; then
+    echo "==================================="
+    echo "Installing jq..."
+    sudo bash -c "$jq_installer"
+fi
 
-which pwsh || \
-    if [[ "$(uname)" == "Darwin" ]]; then
-        brew install --cask powershell
-    else
-        sudo apt-get install -y wget apt-transport-https software-properties-common && \
-        wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb" && \
-        sudo dpkg -i packages-microsoft-prod.deb && \
-        rm packages-microsoft-prod.deb && \
-        sudo apt-get update && \
-        sudo apt-get install -y powershell && \
-        sudo apt-get clean
+if [ ! "$(which bw)" ]; then
+    echo "==================================="
+    echo "Installing Bitwarden CLI..."
+    sudo bash -c "$bitwarden_installer"
+fi
+
+if [ ! "$(which ansible)" ]; then
+    echo "==================================="
+    echo "Installing Ansible..."
+    sudo bash -c "$ansible_installer"
+fi
+
+# Can we clone from github?
+if [ ! "$(ssh github.com 2>&1 | grep success)" ]; then
+    if [ ! "$BW_SESSION" ]; then
+        echo "==================================="
+        echo "Log into Bitwarden..."
+        export BW_SESSION=$(bw unlock --raw || bw login --raw)
     fi
 
+    folder_id=$(bw list folders --search SSH | jq -r '.[] | select(.name=="SSH").id')
+    ssh_secrets=$(bw list items --folderid $folder_id)
 
-[[ -n "$(cat /etc/shells | grep pwsh)" ]] && chsh -s "$(which pwsh)"
+    mkdir -p ~/.ssh
+    echo $ssh_secrets | jq -r '.[] | select(.name=="config").notes' >> ~/.ssh/config
+    echo $ssh_secrets | jq -r '.[] | select(.name=="github_ed25519").login.username' > ~/.ssh/github_ed25519.pub
+    echo $ssh_secrets | jq -r '.[] | select(.name=="github_ed25519").notes' > ~/.ssh/github_ed25519 && chmod 600 ~/.ssh/github_ed25519
 
-which jq || brew install jq
-which bw || brew install bitwarden-cli
+    [[ -n "$(ssh github.com 2>&1 | grep success)" ]] || (echo "SSH auth to github.com failed" && exit 1)
+fi
 
-export BW_SESSION=$(bw unlock --raw || bw login --raw)
+# Clone the ansible playbook repo
+mkdir -p ~/gitroot && pushd ~/gitroot > /dev/null || exit 1
+if [ ! -f ./ansible/run_ansible.sh ]; then
+    git clone ssh://github.com/fsackur/ansible || exit 1
+fi
 
-folder_id=$(bw list folders --search SSH | jq -r '.[] | select(.name=="SSH").id')
-ssh_secrets=$(bw list items --folderid $folder_id)
-echo $ssh_secrets | jq -r '.[] | select(.name=="config").notes' >> ~/.ssh/config
-echo $ssh_secrets | jq -r '.[] | select(.name=="github_ed25519").username' >> ~/.ssh/github_ed25519.pub
-echo $ssh_secrets | jq -r '.[] | select(.name=="github_ed25519").password' >> ~/.ssh/github_ed25519
-
-[[ -n "$(ssh github.com 2>&1 | grep success)" ]] || (echo "SSH auth to github.com failed" && exit 1)
-
-chezmoi init fsackur/dotfiles --ssh --branch chezmoi --recurse-submodules=false --apply
+# Apply the ansible playbook locally
+pushd ansible
+./run_ansible.sh
