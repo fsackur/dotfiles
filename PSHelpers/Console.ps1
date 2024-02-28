c8b68342b64369106e995e3c616233b04b8acd32
Freddie Sackur <github@dustyfox.uk>
2024-02-28 13:08:31 +0000

    Add Get-NetInterfaceName

--- PSHelpers/Console.ps1
+++ PSHelpers/Console.ps1
@@ -473,57 +473,92 @@ function Sync-Chezmoi
 
 if ((-not $IsWindows) -and (Get-Command ip -CommandType Application -ErrorAction Ignore))
 {
+    function Get-NetInterfaceName
+    {
+        [CmdletBinding()]
+        param
+        (
+            [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+            [SupportsWildcards()]
+            [ArgumentCompleter({
+                param ($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+                $Names = Get-NetInterfaceName
+                ($Names -like "$wordToComplete*"), ($Names -like "*$wordToComplete*") | Write-Output | Select-Object -Unique
+            })]
+            [string[]]$Name
+        )
+
+        if ($MyInvocation.ExpectingInput)
+        {
+            $Name = $input
+        }
+
+        $Names = (ip link) -match '^\d' -replace '^\d+: ' -replace ':.*' -replace '@.*'
+        if ($Name)
+        {
+            $Names = $Names | Where-Object {$n = $_; $Name | Where-Object {$n -like $_}}
+        }
+        $Names
+    }
+
     function Get-NetIpAddress
     {
         [CmdletBinding()]
         param
         (
             [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+            [SupportsWildcards()]
+            [ArgumentCompleter({
+                param ($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+                $Names = Get-NetInterfaceName
+                ($Names -like "$wordToComplete*"), ($Names -like "*$wordToComplete*") | Write-Output | Select-Object -Unique
+            })]
             [string[]]$Name
         )
 
-        begin
+        if ($MyInvocation.ExpectingInput)
         {
-            Update-TypeData -Force -TypeName InetAddress -DefaultDisplayPropertySet Name, IpAddress, Prefix, Scope
-            $Pattern = 'inet(6?) (?<IpAddress>\S+)/(?<Prefix>\d+) (brd (?<Broadcast>\S+) )?scope (?<Scope>.*)'
+            $Name = $input
         }
 
-        process
-        {
-            $Name | ForEach-Object {
-                $IpText = ip address show $_ | Out-String
-                $Links = $IpText -split '(?<=^|\n)(?=\d+:)' | ForEach-Object Trim | Where-Object Length
-                $Links | ForEach-Object {
-                    $Head1, $Head2, $Addrs = $_ -split '\n', 3
-                    $Index, $Name, $IfProperties = $Head1 -split ': ', 3
-                    $Hardware = $Head2 -replace '^\s+'
+        $Name = $Name | Get-NetInterfaceName
 
-                    $Addrs -split '\n(?=    inet)' | ForEach-Object Trim | Where-Object Length | ForEach-Object {
-                        $Head, $IpAddressProperties = $_ -split '\n', 2 | ForEach-Object Trim
-                        if ($Head -match $Pattern)
-                        {
-                            $IpAddress = $Matches.IpAddress
-                            $Prefix    = $Matches.Prefix
-                            $Broadcast = $Matches.Broadcast
-                            $Scope     = $Matches.Scope -split ' '
-                        }
-                        else
-                        {
-                            Write-Error "Failed to parse '$Head'"
-                            return
-                        }
+        Update-TypeData -Force -TypeName InetAddress -DefaultDisplayPropertySet Name, IpAddress, Prefix, Scope
+        $Pattern = 'inet(6?) (?<IpAddress>\S+)/(?<Prefix>\d+) (brd (?<Broadcast>\S+) )?scope (?<Scope>.*)'
 
-                        [pscustomobject]@{
-                            PSTypeName          = 'InetAddress'
-                            Index               = $Index
-                            Name                = $Name
-                            IfProperties        = $IfProperties
-                            Hardware            = $Hardware
-                            IpAddress           = [ipaddress]$IpAddress
-                            Prefix              = $Prefix
-                            Scope               = $Scope
-                            IpAddressProperties = $IpAddressProperties.Trim()
-                        }
+        $Name | ForEach-Object {
+            $IpText = ip address show $_ | Out-String
+            $Links = $IpText -split '(?<=^|\n)(?=\d+:)' | ForEach-Object Trim | Where-Object Length
+            $Links | ForEach-Object {
+                $Head1, $Head2, $Addrs = $_ -split '\n', 3
+                $Index, $Name, $IfProperties = $Head1 -split ': ', 3
+                $Hardware = $Head2 -replace '^\s+'
+
+                $Addrs -split '\n(?=    inet)' | ForEach-Object Trim | Where-Object Length | ForEach-Object {
+                    $Head, $IpAddressProperties = $_ -split '\n', 2 | ForEach-Object Trim
+                    if ($Head -match $Pattern)
+                    {
+                        $IpAddress = $Matches.IpAddress
+                        $Prefix    = $Matches.Prefix
+                        $Broadcast = $Matches.Broadcast
+                        $Scope     = $Matches.Scope -split ' '
+                    }
+                    else
+                    {
+                        Write-Error "Failed to parse '$Head'"
+                        return
+                    }
+
+                    [pscustomobject]@{
+                        PSTypeName          = 'InetAddress'
+                        Index               = $Index
+                        Name                = $Name
+                        IfProperties        = $IfProperties
+                        Hardware            = $Hardware
+                        IpAddress           = [ipaddress]$IpAddress
+                        Prefix              = $Prefix
+                        Scope               = $Scope
+                        IpAddressProperties = $IpAddressProperties.Trim()
                     }
                 }
             }
