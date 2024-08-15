ce93307bbc35e6bf76b348a951eff42e4ffb3527
Freddie Sackur <freddie.git@dustyfox.uk>
2024-08-15 12:06:46 +0100

    Use ProfileAsync

--- .chezmoitemplates/profile.ps1
+++ .chezmoitemplates/profile.ps1
@@ -66,12 +66,7 @@ if (Get-Command starship -ErrorAction SilentlyContinue)
 }
 
 
-$env:PWSH_DEFERRED_LOAD = 1
-$LogDeferredLoad = $false
-
-$DeferredLoad = {
-    "dot-sourcing script" | Write-DeferredLoadLog
-
+$AsyncProfile = {
     . "{{ .chezmoi.sourceDir }}/PSHelpers/Console.ps1"
     . "{{ .chezmoi.sourceDir }}/PSHelpers/git_helpers.ps1"
     . "{{ .chezmoi.sourceDir }}/PSHelpers/pipe_operators.ps1"
@@ -83,161 +78,13 @@ $DeferredLoad = {
     {
         . "{{ .chezmoi.sourceDir }}/Work/work_profile.ps1"
     }
-
-    "completed dot-sourcing script" | Write-DeferredLoadLog
 }
 
-function Write-DeferredLoadLog
+if (Import-Module ProfileAsync -PassThru -ea Ignore)
 {
-    param
-    (
-        [Parameter(Mandatory, ValueFromPipeline)]
-        [string]$Message
-    )
-
-    if (-not $LogDeferredLoad) {return}
-
-    $LogPath = if ($env:XDG_CACHE_HOME)
-    {
-        Join-Path $env:XDG_CACHE_HOME PowerShellDeferredLoad.log
-    }
-    else
-    {
-        Join-Path $HOME .cache/PowerShellDeferredLoad.log
-    }
-
-    $Now = [datetime]::Now
-    if (-not $Start)
-    {
-        $Global:Start = $Now
-    }
-
-    $Timestamp = $Now.ToString('o')
-    (
-        $Timestamp,
-        ($Now - $Start).ToString('ss\.fff'),
-        [System.Environment]::CurrentManagedThreadId.ToString().PadLeft(3, ' '),
-        $Message
-    ) -join '  ' | Out-File -FilePath $LogPath -Append
+    Import-ProfileAsync $AsyncProfile -Delay 200
 }
-
-
-if ($env:PWSH_DEFERRED_LOAD -imatch '^(0|false|no)$')
+else
 {
-    . $DeferredLoad
-    return
+    . $AsyncProfile
 }
-
-
-$LogDeferredLoad = $false
-"=== Starting deferred load ===" | Write-DeferredLoadLog
-
-
-# https://seeminglyscience.github.io/powershell/2017/09/30/invocation-operators-states-and-scopes
-$GlobalState = [psmoduleinfo]::new($false)
-$GlobalState.SessionState = $ExecutionContext.SessionState
-
-# A runspace to run our code asynchronously; pass in $Host to support Write-Host
-$Runspace = [runspacefactory]::CreateRunspace($Host)
-$Powershell = [powershell]::Create($Runspace)
-$Runspace.Open()
-$Runspace.SessionStateProxy.PSVariable.Set('GlobalState', $GlobalState)
-
-# ArgumentCompleters are set on the ExecutionContext, not the SessionState
-# Note that $ExecutionContext is not an ExecutionContext, it's an EngineIntrinsics
-$Private = [System.Reflection.BindingFlags]'Instance, NonPublic'
-$ContextField = [System.Management.Automation.EngineIntrinsics].GetField('_context', $Private)
-$GlobalContext = $ContextField.GetValue($ExecutionContext)
-
-# Get the ArgumentCompleters. If null, initialise them.
-$ContextCACProperty = $GlobalContext.GetType().GetProperty('CustomArgumentCompleters', $Private)
-$ContextNACProperty = $GlobalContext.GetType().GetProperty('NativeArgumentCompleters', $Private)
-$CAC = $ContextCACProperty.GetValue($GlobalContext)
-$NAC = $ContextNACProperty.GetValue($GlobalContext)
-if ($null -eq $CAC)
-{
-    $CAC = [System.Collections.Generic.Dictionary[string, scriptblock]]::new()
-    $ContextCACProperty.SetValue($GlobalContext, $CAC)
-}
-if ($null -eq $NAC)
-{
-    $NAC = [System.Collections.Generic.Dictionary[string, scriptblock]]::new()
-    $ContextNACProperty.SetValue($GlobalContext, $NAC)
-}
-
-# Get the AutomationEngine and ExecutionContext of the runspace
-$RSEngineField = $Runspace.GetType().GetField('_engine', $Private)
-$RSEngine = $RSEngineField.GetValue($Runspace)
-$EngineContextField = $RSEngine.GetType().GetFields($Private) | Where-Object {$_.FieldType.Name -eq 'ExecutionContext'}
-$RSContext = $EngineContextField.GetValue($RSEngine)
-
-# Set the runspace to use the global ArgumentCompleters
-$ContextCACProperty.SetValue($RSContext, $CAC)
-$ContextNACProperty.SetValue($RSContext, $NAC)
-
-Remove-Variable -ErrorAction Ignore (
-    'Private',
-    'GlobalContext',
-    'ContextField',
-    'ContextCACProperty',
-    'ContextNACProperty',
-    'CAC',
-    'NAC',
-    'RSEngineField',
-    'RSEngine',
-    'EngineContextField',
-    'RSContext',
-    'Runspace'
-)
-
-$Wrapper = {
-    # Without a sleep, you get issues:
-    #   - occasional crashes
-    #   - prompt not rendered
-    #   - no highlighting
-    # Assumption: this is related to PSReadLine.
-    # 20ms seems to be enough on my machine, but let's be generous - this is non-blocking
-    Start-Sleep -Milliseconds 200
-
-    . $GlobalState {. $DeferredLoad; Remove-Variable DeferredLoad}
-}
-
-$AsyncResult = $Powershell.AddScript($Wrapper.ToString()).BeginInvoke()
-
-$null = Register-ObjectEvent -MessageData $AsyncResult -InputObject $Powershell -EventName InvocationStateChanged -SourceIdentifier __DeferredLoaderCleanup -Action {
-    $AsyncResult = $Event.MessageData
-    $Powershell = $Event.Sender
-    if ($Powershell.InvocationStateInfo.State -ge 2)
-    {
-        if ($Powershell.Streams.Error)
-        {
-            $Powershell.Streams.Error | Out-String | Write-Host -ForegroundColor Red
-        }
-
-        try
-        {
-            # Profiles swallow output; it would be weird to output anything here
-            $null = $Powershell.EndInvoke($AsyncResult)
-        }
-        catch
-        {
-            $_ | Out-String | Write-Host -ForegroundColor Red
-        }
-
-        $h1 = Get-History -Id 1 -ErrorAction Ignore
-        if ($h1.CommandLine -match '\bcode\b.*shellIntegration\.ps1')
-        {
-            $Msg = 'VS Code Shell Integration is enabled. This may cause issues with deferred load. To disable it, set "terminal.integrated.shellIntegration.enabled" to "false" in your settings.'
-            Write-Host $Msg -ForegroundColor Yellow
-        }
-
-        $PowerShell.Dispose()
-        $Runspace.Dispose()
-        Unregister-Event __DeferredLoaderCleanup
-        Get-Job __DeferredLoaderCleanup | Remove-Job
-    }
-}
-
-Remove-Variable Wrapper, Powershell, AsyncResult, GlobalState
-
-"synchronous load complete" | Write-DeferredLoadLog
