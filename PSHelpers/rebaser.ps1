6931f802f3f4449f00c3d4a0d532854b953708a2
Freddie Sackur <freddie.git@dustyfox.uk>
2025-04-24 23:35:17 +0100

    rebaser steps through commits

--- PSHelpers/rebaser.ps1
+++ PSHelpers/rebaser.ps1
@@ -1,158 +1,233 @@
-# function clean {gci *.json, *.txt -Exclude requirements*.txt | del; a | Out-Null; git status -s}
-# function cont {git rebase --continue}
-# function msg {gci ./.git -Filter COMMIT_EDITMSG, MERGE_EDITMSG | % {$_.Name; gc $_ | select -First 8}}
-# function msg {gci .git/COMMIT_EDITMSG, .git/MERGE_EDITMSG -ea Ignore | % {$_.Name; gc $_ | select -First 8}}
-# function tinue {git rebase --continue}
-# function no {gci *.json, *.txt -Exclude requirements*.txt | del; a | Out-Null; git status -s}
-# function msg {gci .git/COMMIT_EDITMSG, .git/MERGE_MSG -ea Ignore | % {$_.Name; gc $_ | select -First 8}}
 
+function Get-GitDir {
+    param (
+        [Parameter(ValueFromPipelineByPropertyName, ValueFromPipeline, Position = 0)]
+        [Alias("Path")]
+        $Dir = $PWD
+    )
+    git -C $GitDir rev-parse --show-toplevel
+}
 
-# $rtd = gc /home/freddie/gitroot/upstream/opnsense/core/.git/rebase-merge/git-rebase-todo
-# $rtd | replace "^pick", "edit" > /home/freddie/gitroot/upstream/opnsense/core/.git/rebase-merge/git-rebase-todo
+function Test-IsRebasing {
+    param ($GitDir = (Get-GitDir))
+    (Test-Path "$GitDir/.git/rebase-merge") -or
+    (Test-Path "$GitDir/.git/rebase-apply")
+}
 
+$RebaseActions = "p", "pick", "r", "reword", "e", "edit", "s", "squash", "f", "fixup", "x", "exec", "b", "break", "d", "drop", "l", "label", "t", "reset", "m", "merge", "u", "update-ref"
+$ActionPattern = "(?<Action>$($RebaseActions -join '|')(\s+-[cC])?)"
+$RefPattern = "((?<Ref>[a-fA-F0-9]{7})[a-fA-F0-9]*|(?<Ref>\S+))"
+$RebasePattern = "^\s*$ActionPattern\s+$RefPattern(\s+(?<Message>.*))?\s*$"
 
-$GitDir = git rev-parse --show-toplevel
-$MergeMsgPath = "$GitDir/.git/MERGE_MSG"
+class RebaseCommit
+{
+    [string]$Action
+    [string]$Ref
+    [string]$Message
 
-$DefaultAction = {
-    pushd $GitDir -ErrorAction Stop
-    try
-    {
-        gci *.json, *.txt -Exclude requirements*.txt | del
+    static [RebaseCommit] Parse([string]$Line) {
+        if ($Line -notmatch $Script:RebasePattern) {
+            throw "Could not parse '$Line"
+        }
+        0..6 | % {$Matches.Remove($_)}
+        return [RebaseCommit]$Matches
     }
-    finally
-    {
-        popd
+
+    [string] ToString() {
+        return $this.Action, $this.Ref, $this.Message -join " "
     }
 }
 
-function tinue {
+class RebaseStatus
+{
+    [int]$Current
+    [int]$Total
+    [RebaseCommit[]]$Done
+    [RebaseCommit[]]$ToDo
+
+    [string] ToString() {
+        return "($($this.Current)/$($this.Total)) $($this.ToDo[0])"
+    }
+}
+
+function Get-RebaseStatus {
+    param ($GitDir = (Get-GitDir))
+    if (-not (Test-Path "$GitDir/.git/rebase-merge/")) {
+        return
+        # TODO:
+        #     "$GitDir/.git/rebase-apply/next", "$GitDir/.git/rebase-apply/last"
+    }
+    [RebaseStatus]@{
+        Current = Get-Content "$GitDir/.git/rebase-merge/msgnum"
+        Total = Get-Content "$GitDir/.git/rebase-merge/end"
+        Done = Get-Content "$GitDir/.git/rebase-merge/done" | % {[RebaseCommit]::Parse($_)}
+        ToDo = Get-Content "$GitDir/.git/rebase-merge/git-rebase-todo" | % {[RebaseCommit]::Parse($_)}
+    }
+}
+
+function Step-Rebase {
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "High")]
-    # bool ShouldContinue(string query, string caption)
-    # bool ShouldContinue(string query, string caption, [ref] bool yesToAll, [ref] bool noToAll)
-    # bool ShouldContinue(string query, string caption, bool hasSecurityImpact, [ref] bool yesToAll, [ref] bool noToAll)
-    # bool ShouldProcess(string target)
-    # bool ShouldProcess(string target, string action)
-    # bool ShouldProcess(string verboseDescription, string verboseWarning, string caption)
-    # bool ShouldProcess(string verboseDescription, string verboseWarning, string caption, [ref] System.Management.Automation.ShouldProcessReason shouldProcessReason)
     param
     (
-        [scriptblock]$Action = $DefaultAction
+        [scriptblock]$Action,
+
+        [string]$GitDir = (Get-GitDir)
     )
 
-    # git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+    if (-not (Test-IsRebasing)) {return}
+    $null = git -C $GitDir -c color.ui=always -c core.editor=true rebase --continue *>&1
+    if (-not (Test-IsRebasing)) {return}
 
-    # $StarshipConfig = starship print-config character.format -d | Parse-IniConf -Unquote
-    # $PromptFormat = $StarshipConfig.character.format
-    try
-    {
-        $RebaseOutput = git -c color.ui=always -c core.editor=true rebase --continue
+    $Status = Get-RebaseStatus
+    $IsConflict = Test-Path "$GitDir/.git/MERGE_MSG"
+    if (-not $IsConflict) {
+        $Status -as [string]
 
-        $IsConflict = Test-Path $MergeMsgPath
-        if ($IsConflict) {
-            $RebaseOutput
+        if ($Status.Done[-1].Action -in "e", "edit") {
+            "You can amend the commit now. Type 'exit' when done."
+            $Host.EnterNestedPrompt()
+        }
 
-            $Changes = (git -c color.ui=always status -s) -replace '^\s+' -replace '\s+', " "
-            if ($PSCmdlet.ShouldProcess($Changes, "merge conflicts")) {
-                $ChangedFiles = $Changes -replace '^\S+\s+' | Resolve-Path -ErrorAction Ignore
-                if ($ChangedFiles) {
-                    $Markers = $ChangedFiles | gci | sls -Pattern "======="
-                    if ($Markers) {
-                        if (-not $PSCmdlet.ShouldProcess($changes, "merge conflicts")) {
-                            return
-                        }
+    } else {
+        "Conflict in $($Status.Ref) $($Status.Message)"
+        $Changes = (git -C $GitDir -c color.ui=always status -s) -replace '^\s+' -replace '\s+', " "
+        if ($PSCmdlet.ShouldProcess($Changes, "merge conflicts")) {
+            $ChangedFiles = $Changes -replace '^\S+\s+' | Resolve-Path -ErrorAction Ignore
+            if ($ChangedFiles) {
+                $Markers = $ChangedFiles | Get-ChildItem | Select-String "<<<<<<"
+                if ($Markers) {
+                    if (-not $PSCmdlet.ShouldProcess($Markers, "merge in spite of conflicts")) {
+                        return
                     }
                 }
-
-                git add $GitDir
-                return & $MyInvocation.MyCommand
-            } else {
-                return
             }
-        }
 
+            git -C $GitDir add $GitDir
+            return & $MyInvocation.MyCommand @PSBoundParameters
+        } else {
+            return
+        }
+    }
+
+    if ($Action) {
         & $Action
+    }
 
-        $Changes = (git -c color.ui=always status -s) -replace '^\s+' -replace '\s+', " "
-        if ($Changes) {
-            $Changes
-            # $LastMessage = git show -s --format=%s HEAD
-            $LastMessage = gc "$GitDir/.git/rebase-merge/message"
-            if ($PSCmdlet.ShouldProcess($changes, "amend commit '$LastMessage'")) {
-                git add $GitDir
-                git commit --amend --no-edit
-                return & $MyInvocation.MyCommand
-            } else {
-                return
-            }
+    $Changes = (git -C $GitDir -c color.ui=always status -s) -replace '^\s+' -replace '\s+', " "
+    if ($Changes) {
+        $Changes
+        $LastMessage = Get-Content "$GitDir/.git/rebase-merge/message"
+        if ($PSCmdlet.ShouldProcess($changes, "amend commit '$LastMessage'")) {
+            git -C $GitDir add $GitDir
+            $null = git -C $GitDir commit --amend --no-edit *>&1
+            return & $MyInvocation.MyCommand
+        } else {
+            return
         }
-
-        return & $MyInvocation.MyCommand
     }
-    finally
-    {
 
-    }
+    return & $MyInvocation.MyCommand @PSBoundParameters
 }
 
-<#
-[Microsoft.PowerShell.PSConsoleReadLine].GetMethods().Name | clip
-Abort
-AcceptAndGetNext
-AcceptLine
+function rebase {
+    [CmdletBinding(DefaultParameterSetName = "Invoke")]
+    param
+    (
+        [Parameter(ParameterSetName = "Mode", Mandatory, Position = 0)]
+        [ValidateSet("continue", "skip", "abort", "quit", "edit-todo", "show-current-patch")]
+        [string]$Command,
 
-AddLine
-AddToHistory
+        [Parameter(ParameterSetName = "Invoke", Mandatory, Position = 0)]
+        [ArgumentCompleter({
+            param ($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
 
-BeginningOfLine
-CaptureScreen
-ClearScreen
-Complete
+            $DirArgs = @{GitDir = if ($fakeBoundParameters.GitDir) {$fakeBoundParameters.GitDir} else {Get-GitDir}}
 
-DigitArgument
-Ding
+            if (Test-IsRebasing @DirArgs) {
+                $word = $wordToComplete -replace '^-+'
+                $Commands = "continue", "skip", "abort", "quit", "edit-todo", "show-current-patch"
+                ($Commands -like "$word*"), ($Commands -like "*$word*") | Write-Output | Select-Object -Unique
 
-EndOfHistory
-EndOfLine
+            } elseif ($wordToComplete -match '^\d+$') {
+                Write-Host "foo"
+                @("HEAD$wordToComplete")
 
-GetDefaultAddToHistoryOption
-GetDisplayGrouping
-GetHistoryItems
+            } else {
+                $Refs = (git -C $GitDir show-ref)[0..15] -replace '.* refs/\w+/'
+                ($Refs -like "$word*"), ($Refs -like "*$word*") | Write-Output | Select-Object -Unique
+            }
+        })]
+        [string]$Upstream,
 
-GotoFirstNonBlankOfLine
-HistorySearchBackward
-HistorySearchForward
-Insert
-Insert
+        [Parameter(ParameterSetName = "Invoke")]
+        [string]$Branch,
 
-InvertCase
+        [Parameter(ParameterSetName = "Invoke")]
+        [string]$Onto,
 
-InvokePrompt
+        [scriptblock]$Exec,
 
-PossibleCompletions
+        [Parameter(ParameterSetName = "Invoke")]
+        [Alias("keep-empty")]
+        [switch]$KeepEmpty,
 
-ReadLine
-ReadLine
+        [Parameter(ParameterSetName = "Invoke")]
+        [switch]$Interactive,
 
-RevertLine
+        [Parameter(ParameterSetName = "Invoke")]
+        [switch]$Autosquash,
 
-ScrollDisplayDown
-ScrollDisplayDownLine
-ScrollDisplayToCursor
-ScrollDisplayTop
-ScrollDisplayUp
-ScrollDisplayUpLine
+        [Parameter(ParameterSetName = "Invoke")]
+        [switch]$Autostash,
 
-SetCursorPosition
+        [Alias("Path", "RepoPath")]
+        [string]$GitDir = (Get-GitDir)
+    )
 
-ShowCommandHelp
+    [string[]]$RebaseArgs = @()
 
-ShowKeyBindings
-ShowParameterHelp
-SwapCharacters
-SwitchPredictionView
-TabCompleteNext
-TabCompletePrevious
-TryGetArgAsInt
-ValidateAndAcceptLine
-#>
+    if ($PSCmdlet.ParameterSetName -eq "Mode") {
+        if (-not (Test-IsRebasing)) {
+            throw "Not rebasing"
+        }
+
+        $RebaseArgs = @("--$Command")
+
+    } else {
+        if (Test-IsRebasing) {
+            $Command = $Upstream -replace "^-+"
+            return & $MyInvocation.MyCommand -Command $Command
+        }
+
+        [void]$PSBoundParameters.Remove("Upstream")
+        [void]$PSBoundParameters.Remove("Branch")
+        [void]$PSBoundParameters.Remove("GitDir")
+
+        $StepArgs = @{GitDir = $GitDir}
+        if ($PSBoundParameters.Remove("Exec")) {$StepArgs["Action"] = $Exec}
+
+        $RebaseArgs = $PSBoundParameters.GetEnumerator() | % {
+            $Param = $MyInvocation.MyCommand.Parameters[$_.Key]
+            if ($Param.SwitchParameter -and -not $_.Value) {return}
+
+            $GitName = $Param.Aliases, $Param.Name | Write-Output | Select-Object -First 1
+            "--$($GitName.ToLower())"
+            if ($Param.SwitchParameter) {return}
+
+            $_.Value
+
+        } | Write-Output
+
+        $RebaseArgs += $Upstream
+        if ($Branch) {$RebaseArgs += $Branch}
+    }
+
+    # -c sequence.editor="pwsh -nop -c edit-rebase"
+    if ($Interactive) {
+        $RebaseOutput = git -C $GitDir -c color.ui=always rebase @RebaseArgs *>&1
+        Step-Rebase @StepArgs
+    } else {
+        git -C $GitDir rebase @RebaseArgs
+    }
+
+}
